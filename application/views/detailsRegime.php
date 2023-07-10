8b9d440c4aebb84e9d690929226225476cbf68a9
sitraka29 <117538726+sitraka29@users.noreply.github.com>
2023-07-11 00:29:25 +0300

    pop up front office

--- application/views/detailsRegime.php
+++ application/views/detailsRegime.php
@@ -60,19 +60,19 @@
                 <ul class="list-group">
                     <li class="list-group-item">
                         Plat 1
-                        <a href="<?php echo base_url('user/User/getPlats/1');?>"> <button class="btn btn-primary btn-sm detail-button">Détail du plat</button></a>
+                        <button class="btn btn-primary btn-sm detail-button" data-toggle="modal" data-target="#detailModal" data-plat="Plat 1" data-ingredients="Ingrédient 1, Ingrédient 2, Ingrédient 3">Détail du plat</button>
                     </li>
                     <li class="list-group-item">
                         Plat 2
-                        <button class="btn btn-primary btn-sm detail-button">Détail du plat</button>
+                        <button class="btn btn-primary btn-sm detail-button" data-toggle="modal" data-target="#detailModal" data-plat="Plat 2" data-ingredients="Ingrédient 4, Ingrédient 5, Ingrédient 6">Détail du plat</button>
                     </li>
                     <li class="list-group-item">
                         Plat 3
-                        <button class="btn btn-primary btn-sm detail-button">Détail du plat</button>
+                        <button class="btn btn-primary btn-sm detail-button" data-toggle="modal" data-target="#detailModal" data-plat="Plat 3" data-ingredients="Ingrédient 7, Ingrédient 8, Ingrédient 9">Détail du plat</button>
                     </li>
                     <li class="list-group-item">
                         Plat 4
-                        <button class="btn btn-primary btn-sm detail-button">Détail du plat</button>
+                        <button class="btn btn-primary btn-sm detail-button" data-toggle="modal" data-target="#detailModal" data-plat="Plat 4" data-ingredients="Ingrédient 10, Ingrédient 11, Ingrédient 12">Détail du plat</button>
                     </li>
                 </ul>
             </div>
@@ -90,6 +90,43 @@
             </div>
         </div>
     </div>
+    
+    <div id="detailModal" class="modal fade">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Détail du plat</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <h4 id="platTitle"></h4>
+                    <ul id="ingredientList" class="list-group"></ul>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script>
+        $('#detailModal').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget);
+            var plat = button.data('plat');
+            var ingredients = button.data('ingredients').split(', ');
+            var modal = $(this);
+            modal.find('#platTitle').text(plat);
+            modal.find('#ingredientList').empty();
+            ingredients.forEach(function (ingredient) {
+                var listItem = $('<li>', { class: 'list-group-item', text: ingredient });
+                modal.find('#ingredientList').append(listItem);
+            });
+        });
+    </script>
 </body>
 </html>
